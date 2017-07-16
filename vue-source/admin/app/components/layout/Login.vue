01d6dbb579e1c1c394382bfaa35f7135b5ab7414
李铭昕 <715557344@qq.com>
2017-07-16 15:18:31 +0800

    * 登录

--- vue-source/admin/app/components/layout/Login.vue
+++ vue-source/admin/app/components/layout/Login.vue
@@ -1,16 +1,18 @@
 <template>
-    <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="用户名">
-            <el-input v-model="form.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-            <el-input v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">登录</el-button>
-            <el-button>取消</el-button>
-        </el-form-item>
-    </el-form>
+    <div class="login">
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="用户名">
+                <el-input v-model="form.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="form.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">登录</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 <script>
     export default {
