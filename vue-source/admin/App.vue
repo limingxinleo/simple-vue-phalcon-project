548a4f64e2e42b4aa1eda02fc87070af3c6ecc9b
李铭昕 <715557344@qq.com>
2017-07-16 14:22:13 +0800

    * 修改admin布局

--- vue-source/admin/App.vue
+++ vue-source/admin/App.vue
@@ -1,10 +1,27 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <layout-header></layout-header>
+        <el-row class="tac">
+            <el-col :span="4">
+                <layout-sidebar></layout-sidebar>
+            </el-col>
+            <el-col :span="20">
+                <div class="container">
+                    <router-view></router-view>
+                </div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script>
+    import Header from './app/components/layout/Header.vue';
+    import SideBar from './app/components/layout/SideBar.vue';
+    import './common/css/style.css';
     export default {
+        components: {
+            'layout-header': Header,
+            'layout-sidebar': SideBar,
+        },
         name: 'app'
     }
 </script>
---
+++ vue-source/admin/app/components/layout/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-menu id="layout-sidebar" default-active="2" class="el-menu-vertical-demo" @open="handleOpen"
+             @close="handleClose">
+        <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
+        <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+    </el-menu>
+</template>
+<script>
+    export default {
+        name: 'layout-sidebar',
+        data () {
+            return {};
+        },
+        mounted: function () {
+            console.log('SideBar mounted!');
+        }
+    }
+</script>
